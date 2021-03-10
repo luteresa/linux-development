de0a68a92b2901968c0c4fc036e531027ef2358d
leon <luteresa@163.com>
2021-03-10 15:14:51 +0800

    1.add arm manual; 2.add mm test file;

---
+++ mm/mm.cpp
@@ -0,0 +1,24 @@
+/*****************************************
+function:
+param:
+return:
+creat by:leon
+local time:Fri Mar  5 16:51:23 2021
+*****************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unistd.h>
+  
+using namespace std;
+  
+int main(int argc, const char *argv[])
+{
+  
+	int t[10];
+
+	cout<<"start..."<<endl;
+	sleep(60);
+  
+	 return 0;
+}
